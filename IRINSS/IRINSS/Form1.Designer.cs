e86576a32cdd65983793b52f524f301f80d354ff
Nemésio Florence Vieira Filho <54808745+NemesioFVF@users.noreply.github.com>
2021-02-01 21:17:33 -0300

    Add files via upload

---
+++ IRINSS/IRINSS/Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace IRINSS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label13 = new System.Windows.Forms.Label();
+            this.lblSL = new System.Windows.Forms.Label();
+            this.lblIR = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblINSS = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.salBruto = new System.Windows.Forms.TextBox();
+            this.depend = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel4.Location = new System.Drawing.Point(12, 311);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(629, 10);
+            this.panel4.TabIndex = 11;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel5.Location = new System.Drawing.Point(11, 358);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(630, 10);
+            this.panel5.TabIndex = 10;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label13.Location = new System.Drawing.Point(262, 158);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(35, 23);
+            this.label13.TabIndex = 18;
+            this.label13.Text = "R$";
+            // 
+            // lblSL
+            // 
+            this.lblSL.AutoSize = true;
+            this.lblSL.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSL.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblSL.Location = new System.Drawing.Point(211, 390);
+            this.lblSL.Name = "lblSL";
+            this.lblSL.Size = new System.Drawing.Size(0, 23);
+            this.lblSL.TabIndex = 17;
+            // 
+            // lblIR
+            // 
+            this.lblIR.AutoSize = true;
+            this.lblIR.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIR.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblIR.Location = new System.Drawing.Point(56, 463);
+            this.lblIR.Name = "lblIR";
+            this.lblIR.Size = new System.Drawing.Size(0, 23);
+            this.lblIR.TabIndex = 15;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.Red;
+            this.label9.Location = new System.Drawing.Point(83, 406);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 23);
+            this.label9.TabIndex = 14;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel5);
+            this.panel1.Controls.Add(this.label13);
+            this.panel1.Controls.Add(this.lblSL);
+            this.panel1.Controls.Add(this.lblINSS);
+            this.panel1.Controls.Add(this.lblIR);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.salBruto);
+            this.panel1.Controls.Add(this.depend);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(653, 510);
+            this.panel1.TabIndex = 1;
+            // 
+            // lblINSS
+            // 
+            this.lblINSS.AutoSize = true;
+            this.lblINSS.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblINSS.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblINSS.Location = new System.Drawing.Point(83, 427);
+            this.lblINSS.Name = "lblINSS";
+            this.lblINSS.Size = new System.Drawing.Size(0, 23);
+            this.lblINSS.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(211, 369);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 23);
+            this.label8.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label7.Location = new System.Drawing.Point(23, 390);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(191, 23);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "SALÁRIO LÍQUIDO:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label6.Location = new System.Drawing.Point(24, 463);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 23);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "IR:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(24, 427);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 23);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "INSS:";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel3.Location = new System.Drawing.Point(0, 21);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(653, 10);
+            this.panel3.TabIndex = 9;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel2.Location = new System.Drawing.Point(0, 80);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(653, 10);
+            this.panel2.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label5.Location = new System.Drawing.Point(35, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(579, 42);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "CÁLCULO DO SALÁRIO LÍQUIDO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label3.Location = new System.Drawing.Point(81, 324);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(506, 31);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "RESULTADOS COM BASE EM MARÇO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(14, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(179, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Digite salario bruto:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(14, 217);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(282, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Digite o número de dependentes:";
+            // 
+            // salBruto
+            // 
+            this.salBruto.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.salBruto.Location = new System.Drawing.Point(303, 152);
+            this.salBruto.Name = "salBruto";
+            this.salBruto.Size = new System.Drawing.Size(90, 35);
+            this.salBruto.TabIndex = 2;
+            // 
+            // depend
+            // 
+            this.depend.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.depend.Location = new System.Drawing.Point(303, 211);
+            this.depend.Name = "depend";
+            this.depend.Size = new System.Drawing.Size(90, 35);
+            this.depend.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(416, 134);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(213, 133);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CALCULAR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(676, 534);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "CÁLCULO SALÁRIO LÍQUIDO";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label lblSL;
+        private System.Windows.Forms.Label lblIR;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblINSS;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox salBruto;
+        private System.Windows.Forms.TextBox depend;
+        private System.Windows.Forms.Button button1;
+
+    }
+}
+
---
+++ IRINSS/IRINSS/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace IRINSS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Double sb, inss, ir, irrf=0, salb, sl, ndp;
+            String esl, einss, eir;
+            sb = Convert.ToDouble(salBruto.Text);
+            if (sb <= 1317.07)
+            {
+                inss = sb * 0.08;
+            }
+            else if (sb <= 2195.12)
+            {
+                inss = sb * 0.09;
+            }
+            else
+            {
+                inss = sb * 0.11;
+            }
+
+            if (inss > 482.93)
+            {
+                inss = 482.93;
+            }
+
+            ndp = Convert.ToDouble(depend.Text);
+
+            salb = sb - inss - (179.71 * ndp);
+
+            if (salb <= 1787.77)
+            {
+                ir = 0;
+            }
+            else if (salb <= 2679.29)
+            {
+                ir = salb * 0.075;
+                irrf = ir - 134.08;
+            }
+            else if (salb <= 3572.43)
+            {
+                ir = salb * 0.15;
+                irrf = ir - 335.08;
+            }
+            else if (salb <= 4463.81)
+            {
+                ir = salb * 0.225;
+                irrf = ir - 602.96;
+            }
+            else
+            {
+                ir = salb * 0.275;
+                irrf = ir - 826.15;
+            }
+
+            sl = sb - inss - irrf;
+            esl = Convert.ToString(sl);
+            lblSL.Text = esl;
+            einss = Convert.ToString(inss);
+            lblINSS.Text = einss;
+            eir = Convert.ToString(irrf);
+            lblIR.Text = eir;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
